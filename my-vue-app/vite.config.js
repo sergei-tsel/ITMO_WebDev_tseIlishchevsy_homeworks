e08e74236b517f3b06e4d45abaddb891b371d6d4
sergi-tsel <tselishchevsy@gmail.com>
2022-10-28 17:26:18 +0300

    create workspace

---
+++ my-vue-app/vite.config.js
@@ -0,0 +1,19 @@
+/** @type {import('vite').UserConfig} */
+import path from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  server: {
+    port: 5173,
+    // host: 'local.dev'
+    // https: true,
+  }
+})
